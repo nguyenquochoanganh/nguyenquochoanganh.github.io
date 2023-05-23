89b9e38184ed9c2ec99f70dd489e442e62dc3c27
Nguyenthuan <69764143+thuanpham113@users.noreply.github.com>
2023-05-24 02:53:27 +0700

    e789e9c

---
+++ _nuxt/static/1684871456/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCV\u002F_nuxt\u002Fstatic\u002F1684871456",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002FCV\u002F",assetsPath:"\u002FCV\u002F_nuxt\u002F",cdnURL:a}}}}(null));
